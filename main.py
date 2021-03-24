eee7c21230603c98b102bb1e909dfa247e7b37f5
Anton <a.khilman@bff.by>
2021-03-24 20:23:44 +0300

    add gitignore

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+boolean_var_True = True
+boolean_var_False = False
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Hello_message = 'hello world'
+even_number = (2, hello_message, boolean_var_True, 3.5, 18)
+print(list(even_number))
 
+even_number = even_number + [4, 5, 6]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(list(even_number))
